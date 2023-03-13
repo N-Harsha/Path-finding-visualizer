fc77b51d7cd62a28a9cc07487f0fc329cccebc59
N-Harsha <harsha0770@gmail.com>
2023-03-13 13:29:07 +0530

    deploy branch created

--- src/bfs.js
+++ src/bfs.js
@@ -1,5 +1,5 @@
-import paint from "/src/paint.js";
-import { Queue, Node } from "/src/Queue.js";
+import paint from "./src/paint.js";
+import { Queue, Node } from "./src/Queue.js";
 
 let dest = [];
 let row = 1;
